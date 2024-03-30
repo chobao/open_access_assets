d2a93ba062c93c598021f4bc7cb3f1bb1d1d9979
Bao <b757508036@126.com>
2024-03-30 01:03:00 +0100

    reduce geneavatar

--- geneavatar/images/pdf2png.py
+++
@@ -1,8 +0,0 @@
-# import module
-from pdf2image import convert_from_path
- 
-name = 'framework'
-# Store Pdf with convert_from_path function
-images = convert_from_path(f'./{name}.pdf', dpi=500)
- 
-images[0].save(f'{name}.png', 'PNG')
